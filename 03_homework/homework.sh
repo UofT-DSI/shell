af57a66985b42f821fff4fbf58318243c1bab4b9
tarunvashisht <167667529+tarunvashisht@users.noreply.github.com>
2024-04-23 20:41:23 -0400

    Update homework.sh

--- 03_homework/homework.sh
+++ 03_homework/homework.sh
@@ -7,9 +7,9 @@
 # On your terminal, input all the commands you have used to create the following:
 
 # 1. How would you create 5 directories? Feel free to use any name for your directories.
-
+A. mkdir dir1 dir2 dir3 dir4 dir5
 # 2. How would you verify the creation of all 5 directories?
-
+ls or use TAB on the root folder where command was done
 # 3. In each directory, how would you create 5 .txt files and write "I love data" into each within the directories?
 
 # 4. How would you verify the presence of all 5 files?
