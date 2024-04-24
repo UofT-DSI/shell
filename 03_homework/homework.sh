fc21b24de5ac0d53a8ae45fddb508fb1e01e1d4e
Sarita Rana <34283419+sari-tech@users.noreply.github.com>
2024-04-24 18:14:24 -0400

    Update homework.sh

--- 03_homework/homework.sh
+++ 03_homework/homework.sh
@@ -7,11 +7,11 @@
 # On your terminal, input all the commands you have used to create the following:
 
 # 1. How would you create 5 directories? Feel free to use any name for your directories.
-
+mkdir dir1 dir2 dir3 dir4 dir5
 # 2. How would you verify the creation of all 5 directories?
-
+ls
 # 3. In each directory, how would you create 5 .txt files and write "I love data" into each within the directories?
-
+touch dir1/file1.txt dir2/file2.txt dir3/file3.txt dir4/file4.txt dir5/file5.txt
 # 4. How would you verify the presence of all 5 files?
 
 # 5. How would you append to one of the existing files " and machine learning!"?
@@ -21,7 +21,8 @@
 # 7. How would you delete all files except for the one with the appended text?
 
 # 8. How would you navigate back to the parent directory containing all the directories?
-
+cd ../
 # 9. How would you remove each directory along with its contents?
-
+rem dir1 dir2 dir3 dir4 dir5
 # 10. How would you verify that all directories and files have been deleted?
+ls
