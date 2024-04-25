cd9f4487fce6a1d93e8ab614b1a1a50b65283108
Okky Rijanto <okky.corp@gmail.com>
2024-04-24 21:09:52 -0400

    test

--- 03_homework/homework.sh
+++ 03_homework/homework.sh
@@ -7,10 +7,16 @@
 # On your terminal, input all the commands you have used to create the following:
 
 # 1. How would you create 5 directories? Feel free to use any name for your directories.
+mkdir dir1 dir2 dir3 dir4 dir5
 
 # 2. How would you verify the creation of all 5 directories?
-
+ls -1 dir1 dir2 dir3 dir4 dir5
 # 3. In each directory, how would you create 5 .txt files and write "I love data" into each within the directories?
+for dir in dir{1..5}; do
+  for i in {1..5}; do
+    echo "I love DSI" > "$dir/file$i.txt"
+  done
+done
 
 # 4. How would you verify the presence of all 5 files?
 
