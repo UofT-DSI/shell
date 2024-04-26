d390dd562c6757374d2c76574d939b242a7293a4
Kristina Talalaievska <149816236+KristinaTalalaievska@users.noreply.github.com>
2024-04-25 23:43:44 -0400

    Update homework.sh

--- 03_homework/homework.sh
+++ 03_homework/homework.sh
@@ -7,21 +7,35 @@
 # On your terminal, input all the commands you have used to create the following:
 
 # 1. How would you create 5 directories? Feel free to use any name for your directories.
+mkdir Dir1 Dir2 Dir3 Dir4 Dir5
 
 # 2. How would you verify the creation of all 5 directories?
+ls -a
 
 # 3. In each directory, how would you create 5 .txt files and write "I love data" into each within the directories?
+ouch 5.txt Dir1 DIr2 Dir3 Dir4 Dir5
+echo "I love data" 
+cp 5.txt or rsync -r  Dir1/ Dir2/ Dir3/ Dir4/ Dir5
 
 # 4. How would you verify the presence of all 5 files?
+ls -a
 
 # 5. How would you append to one of the existing files " and machine learning!"?
+cat >> 5.txt
+“machine learning”
 
 # 6. How would you verify that the text was indeed appended to the existing file?
+cat 5.txt
 
 # 7. How would you delete all files except for the one with the appended text?
+rm -v !(“5.txt”)
 
 # 8. How would you navigate back to the parent directory containing all the directories?
+cd ..
 
 # 9. How would you remove each directory along with its contents?
+rmdir Dir1/ Dir2/ Dir3/ Dir4/ Dir5 
 
 # 10. How would you verify that all directories and files have been deleted?
+ls -a
+echo “I love data” > 5.txt 
