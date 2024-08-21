f4ede1aaa429b9bfc9ee4e10d8e5eeb688b66d75
514ccmtl <lauc1@outlook.com>
2024-08-21 19:17:30 -0400

    Updated assignment.sh as of Aug 21 2024 1920h
    -Added message regarding "main_directory" as parent directory

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -7,6 +7,7 @@ set -x
 # > Edit this file below to complete the homework assignment
 # > Add your code below each comment to complete the tasks
 
+# My parent directory is called main_directory
 # 1. Create 5 directories named dir1, dir2, dir3, dir4, and dir5
 mkdir dir1 dir2 dir3 dir4 dir5
 
