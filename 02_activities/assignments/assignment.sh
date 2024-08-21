799a7034b271e6a4ba3f1ad20e4c901601dba2f4
Obiudaku <140523138+Obiudaku@users.noreply.github.com>
2024-08-21 18:57:41 -0400

    Update assignment.sh added command to create 5 directories
    Question 1 from the assignment

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -8,6 +8,7 @@ set -x
 # > Add your code below each comment to complete the tasks
 
 # 1. Create 5 directories named dir1, dir2, dir3, dir4, and dir5
+mkdr dir1 dir2 dir3 dir4 dir5
 
 # 2. List the contents of the parent directory to verify the presence of the 5 directories
 
