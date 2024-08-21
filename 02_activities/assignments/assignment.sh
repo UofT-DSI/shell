51875f393ec0e5b076696fc51434f03ff6e23ab1
Pavlo Danylchenko <danilchenko@ukr.net>
2024-08-21 19:05:30 -0400

    Update assignment.sh
    Part of the assignment

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -8,10 +8,13 @@ set -x
 # > Add your code below each comment to complete the tasks
 
 # 1. Create 5 directories named dir1, dir2, dir3, dir4, and dir5
+mkdir dir1 dir2 dir3 dir4 dir5
 
 # 2. List the contents of the parent directory to verify the presence of the 5 directories
+ls
 
 # 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
+touch dir2/file1 dir2/file2 dir2/file3 dir2/file4 dir2/file5
 
 # 4. Append the words "Hello world" to dir2/file3
 
