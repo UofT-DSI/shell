06f1f0e3aab83c9a137660a3cc8bfd3d9cc462e7
bnusir <bnusir@gmail.com>
2024-08-22 16:32:08 -0400

    Update assignment.sh
    Solved all question

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -18,12 +18,16 @@ cd dir2
 touch file1 file2 file3 file4 file5
 
 # 4. Append the words "Hello world" to dir2/file3
-
+echo "Hello world" >> /dir2/file3
 
 # 5. Verify that file3 contains the words "hello world" by printing the contents of the file in the terminal
+cat /dir2/file3
 
 # 6. Delete file4
+rm /dir2/file2
 
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
+rm -r dir4 dir5
 
 # 8. List the contents of the parent directory to verify the deletion of dir4 and dir5
+ls
