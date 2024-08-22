a408eff249c58a6193af14362b278b836bbd7043
Daya709 <Khadka.daya99@gmail.com>
2024-08-21 22:18:40 -0400

    Update assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -13,7 +13,7 @@ mkdir dir1 dir2 dir3 dir4 dir5
 ls
 # 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
 cd dir2
-touch file1.txt file2.txt file3.txt file4.txt file5.txt
+touch file1 file2 file3 file4 file5
 # 4. Append the words "Hello world" to dir2/file3
 echo 'Hello World' > dir2/file3
 # 5. Verify that file3 contains the words "hello world" by printing the contents of the file in the terminal
