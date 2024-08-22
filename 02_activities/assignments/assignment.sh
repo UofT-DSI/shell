b1f6eaa4d2c487938f948a4bc028080a52f44143
514ccmtl <lauc1@outlook.com>
2024-08-22 19:49:05 -0400

    Update assignment.sh Aug 22 1950
    - Changed answer to Q #3 , 7

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -7,6 +7,8 @@ set -x
 # > Edit this file below to complete the homework assignment
 # > Add your code below each comment to complete the tasks
 
+# My main working directory is home_directory
+
 # 1. Create 5 directories named dir1, dir2, dir3, dir4, and dir5
 mkdir dir1 dir2 dir3 dir4 dir5
 
@@ -14,11 +16,12 @@ mkdir dir1 dir2 dir3 dir4 dir5
 ls
 
 # 3. Create 5 text files in dir2 named file1.txt, file2.txt, file3.txt, file4.txt, and file5.txt
-touch dir2/file1.txt 
-touch dir2/file2.txt 
-touch dir2/file3.txt 
-touch dir2/file4.tx 
-touch dir2/file5.txt
+cd dir2
+touch file1.txt 
+touch file2.txt 
+touch file3.txt 
+touch file4.tx 
+touch file5.txt
 
 # 4. Append the words "Hello world" to dir2/file3.txt
 echo "Hello world" >> dir2/file3.txt
@@ -30,7 +33,7 @@ cat dir2/file3.txt
 rm dir2/file4.txt
 
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
-cd ..
+cd home_directory
 rm -r dir4 dir5 
 
 # 8. List the contents of the current working directory to verify the deletion of dir4 and dir5
