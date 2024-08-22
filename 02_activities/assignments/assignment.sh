0f7c9c123c8bbb2e476d2122589ec9ac4f092340
514ccmtl <lauc1@outlook.com>
2024-08-21 22:08:55 -0400

    Update assignment.sh Aug 21 2208h
    - Removed parent directory as "main_directory"
    - Realized I did not enter .txt for file1 to file5, so revised
    - Revised .txt files for Q#3-6
    - Used cd .. as command in Q#7

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -7,8 +7,6 @@ set -x
 # > Edit this file below to complete the homework assignment
 # > Add your code below each comment to complete the tasks
 
-# My parent directory is called main_directory
-
 # 1. Create 5 directories named dir1, dir2, dir3, dir4, and dir5
 mkdir dir1 dir2 dir3 dir4 dir5
 
@@ -16,20 +14,21 @@ mkdir dir1 dir2 dir3 dir4 dir5
 ls
 
 # 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
-touch dir2/file1 dir2/file2 dir2/file3 dir2/file4 dir2/file5
+cd dir2
+touch file1.txt file2.txt file3.txt file4.txt file5.txt
 
 # 4. Append the words "Hello world" to dir2/file3
-echo "Hello world" >> dir2/file3
+echo "Hello world" >> file3.txt
 
 # 5. Verify that file3 contains the words "hello world" by printing the contents of the file in the terminal
-cat dir2/file3
+cat file3.txt
 
 # 6. Delete file4
-rm -r dir2/file4
+rm -r file4.txt
 
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
-cd main_directory
-rm -r dir4 dir5
+cd ..
+rm -rf dir4 dir5
 
 # 8. List the contents of the parent directory to verify the deletion of dir4 and dir5
 ls
