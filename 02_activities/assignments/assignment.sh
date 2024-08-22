fb28e37209871490ce1e5fcb95901956953f0d3f
514ccmtl <lauc1@outlook.com>
2024-08-22 19:43:53 -0400

    Update assignment.sh Aug 22 1945h
    - Changed answers to Q#3, 4, 5, 6

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -14,17 +14,20 @@ mkdir dir1 dir2 dir3 dir4 dir5
 ls
 
 # 3. Create 5 text files in dir2 named file1.txt, file2.txt, file3.txt, file4.txt, and file5.txt
-cd dir2
-touch file1.txt file2.txt file3.txt file4.tx file5.txt
+touch dir2/file1.txt 
+touch dir2/file2.txt 
+touch dir2/file3.txt 
+touch dir2/file4.tx 
+touch dir2/file5.txt
 
 # 4. Append the words "Hello world" to dir2/file3.txt
-echo "Hello world" >> file3.txt
+echo "Hello world" >> dir2/file3.txt
 
 # 5. Verify that file3.txt contains the words "hello world" by printing the contents of the file in the terminal
-cat file3.txt
+cat dir2/file3.txt
 
 # 6. Delete file4.txt
-rm file4.txt
+rm dir2/file4.txt
 
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
 cd ..
