bb90862638b7e33bc259bd5a6f5d79053289b354
amlloren <adrienne.lloren@gmail.com>
2024-08-21 21:00:47 -0400

    Updated answers to my assignment

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -18,13 +18,13 @@ cd dir2
 touch file1 file2 file3 file4 file5
 
 # 4. Append the words "Hello world" to dir2/file3
-echo "Hello world" >> file3
+echo "Hello world" >> dir2/file3
 
 # 5. Verify that file3 contains the words "hello world" by printing the contents of the file in the terminal
-cat file3
+cat dir2/file3
 
 # 6. Delete file4
-rm file4
+rm dir2/file4
 
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
 cd ../
