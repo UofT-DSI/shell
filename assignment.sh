6e2f8ad478af1574b945171c0f5f63e0e21645b7
Kathryn Vozoris <kvozoris@outlook.com>
2024-08-23 12:24:16 -0400

    Answered first 4 questions and deleted extra text

--- assignment.sh
+++ assignment.sh
@@ -23,17 +23,3 @@ echo "Hello world" >> dir2/file3
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
 
 # 8. List the contents of the parent directory to verify the deletion of dir4 and dir5
-
-# 2. List the contents of the current working directory to verify the presence of the 5 directories
-ls
-# 3. Create 5 text files in dir2 named file1.txt, file2.txt, file3.txt, file4.txt, and file5.txt
-cd dir2
-touch file1.txt file2.txt file3.txt file4.txt file5.txt
-# 4. Append the words "Hello world" to dir2/file3.txt
-
-# 5. Verify that file3.txt contains the words "hello world" by printing the contents of the file in the terminal
-
-# 6. Delete file4.txt
-
-# 7. Delete directories dir4 and dir5 including all their contents (if any)
-# 8. List the contents of the current working directory to verify the deletion of dir4 and dir5
