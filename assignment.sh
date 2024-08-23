0ab040fdf4de5d57cafc8b5bdc66ef467c69f386
Kathryn Vozoris <kvozoris@outlook.com>
2024-08-23 12:18:57 -0400

    "Answere questions 1-4"

--- assignment.sh
+++ assignment.sh
@@ -9,7 +9,6 @@ set -x
 
 # 1. Create 5 directories named dir1, dir2, dir3, dir4, and dir5
 mkdir dir1 dir2 dir3 dir4 dir5
-<<<<<<< HEAD
 # 2. List the contents of the parent directory to verify the presence of the 5 directories
 ls
 # 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
@@ -24,7 +23,6 @@ echo "Hello world" >> dir2/file3
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
 
 # 8. List the contents of the parent directory to verify the deletion of dir4 and dir5
-=======
 
 # 2. List the contents of the current working directory to verify the presence of the 5 directories
 ls
@@ -39,4 +37,3 @@ touch file1.txt file2.txt file3.txt file4.txt file5.txt
 
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
 # 8. List the contents of the current working directory to verify the deletion of dir4 and dir5
->>>>>>> 11fd69412af53d19c52a510b787c9d98280e46cb
