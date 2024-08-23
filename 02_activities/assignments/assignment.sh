46868f6e06f09db7e3c4e2958fbec84162d2cbe4
Tagimasa <alexei.virko@gmail.com>
2024-08-23 18:49:56 -0400

    assignment 1 compleated

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -8,17 +8,18 @@ set -x
 # > Add your code below each comment to complete the tasks
 
 # 1. Create 5 directories named dir1, dir2, dir3, dir4, and dir5
-
+mkdir dir1 dir2 dir3 dir4 dir5
 # 2. List the contents of the current working directory to verify the presence of the 5 directories
-
+ls
 # 3. Create 5 text files in dir2 named file1.txt, file2.txt, file3.txt, file4.txt, and file5.txt
-
+touch dir2/file1.txt dir2/file2.txt dir2/file3.txt dir2/file4.txt dir2/file5.txt
 # 4. Append the words "Hello world" to dir2/file3.txt
-
+echo "Hello world" > dir2/file3.txt
 # 5. Verify that file3.txt contains the words "hello world" by printing the contents of the file in the terminal
-
+cat dir2/file3.txt
 # 6. Delete file4.txt
-
+rm dir2/file4.txt
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
-
+rm -r dir4 dir5
 # 8. List the contents of the current working directory to verify the deletion of dir4 and dir5
+ls
