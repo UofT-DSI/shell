1486f6713ad032f7125fcdaaec5c1db7eb221404
adaobi7 <adaogechi4@gmail.com>
2024-08-24 09:57:59 -0400

    I have added my answers to the assignment questions

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-set -x
-
+ set -x
+m
 ################
 # Instructions #
 ################
@@ -8,17 +8,28 @@ set -x
 # > Add your code below each comment to complete the tasks
 
 # 1. Create 5 directories named dir1, dir2, dir3, dir4, and dir5
-
+ mkdir dir1, mkdir dir2, mkdir dir3, mkdir dir4, mkdir dir5
 # 2. List the contents of the parent directory to verify the presence of the 5 directories
-
+ls
 # 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
+$ touch dir2/file1.txt
+$ touch dir2/file2.txt
+$ touch dir2/file3.txt
+$ touch dir2/file4.txt
+$ touch dir2/file5.txt
+
+
+
 
 # 4. Append the words "Hello world" to dir2/file3
+echo "Hello World" >> dir2/file3.txt
 
 # 5. Verify that file3 contains the words "hello world" by printing the contents of the file in the terminal
+cat dir2/file3.txt
 
 # 6. Delete file4
-
+rm file4.txt
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
-
+rm -r dir2 dir5
 # 8. List the contents of the parent directory to verify the deletion of dir4 and dir5
+ls
