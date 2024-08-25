b66b56b2fbb16789e6af50ffbca4f3e47c0545c1
lisafeeney33 <lisa.feeney@hotmail.com>
2024-08-25 08:35:35 -0400

    Update assignment.sh
    third time is a charm!

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -14,21 +14,19 @@ mkdir dir1 dir2 dir3 dir4 dir5
 ls
 
 # 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
-cd /c/Users/Lisa/Dir2
 Touch file1 file2 file3 file4 file5
 
 # 4. Append the words "Hello world" to dir2/file3
 echo "Hello World" >> dir2/file3.txt
 
 # 5. Verify that file3 contains the words "hello world" by printing the contents of the file in the terminal
-cat file3.txt
+cat dir2/file3.txt
 
 # 6. Delete file4
-$ rm file4 -r
+rm dir2/file4
 
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
 rm dir4 dir5
 
 # 8. List the contents of the parent directory to verify the deletion of dir4 and dir5
-CD ~
 ls
