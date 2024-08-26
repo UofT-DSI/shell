48a0bf5db74269420d94241372835040514d05f8
lisafeeney33 <lisa.feeney@hotmail.com>
2024-08-25 22:02:35 -0400

    Update assignment.sh
    #8

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -14,6 +14,7 @@ mkdir dir1 dir2 dir3 dir4 dir5
 ls
 
 # 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
+cd dir2
 Touch dir2/file1.txt 
 Touch dir2/file2.txt 
 Touch dir2/file3.txt 
