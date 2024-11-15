e976b55a5c2a83f4f2ab1221ea228c9b8737db4f
Simeon Wong <dtxe@users.noreply.github.com>
2024-11-14 20:32:09 -0500

    delete ip logs... WARNING UNTESTED!

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -33,6 +33,7 @@ unzip rawdata.zip
 # 6. Repeat the above step for user logs and event logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+rf -rf ./data
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
