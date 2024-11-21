82ad1355c6a01f61883916aa3620bbdbc3e24388
AbrahimSA <abrahimsabrahao@yahoo.ca>
2024-11-21 18:24:11 -0500

    assignment commands added

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -14,27 +14,36 @@ touch README.md
 touch analysis/main.py
 
 # download client data
+# pip install wget
 wget -O rawdata.zip https://github.com/UofT-DSI/shell/raw/refs/heads/main/02_activities/assignments/rawdata.zip
+#  pip install unzip
 unzip rawdata.zip
 
 ###########################################
 # Complete assignment here
 
 # 1. Create a directory named data
-
+mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
-
+mv ./rawdata ./data/raw
 # 3. List the contents of the ./data/raw directory
-
+ls -ltr ./data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-
+mkdir ./data/processed
+mkdir ./data/processed/server_logs ./data/processed/user_logs  ./data/processed/event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-
+cp ./data/raw/*server*.log ./data/processed/server_logs/
 # 6. Repeat the above step for user logs and event logs
+cp ./data/raw/*user*.log ./data/processed/user_logs/
+cp ./data/raw/*event*.log ./data/processed/event_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+find  ./data/raw -name '*ipaddr*' -exec rm {} \;
+find  ./data/processed/user_logs -name '*ipaddr*' -exec rm {} \;
+
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
+find ./data/processed/ -name "*.log"  -printf "%f\n" > ./data/inventory.txt
 
 
 
