157bb90e44ceab6de428dba83ba1e6d1a6fe75f1
Hassam <saadehassan@hotmail.com>
2024-11-20 23:44:34 -0500

    Assignment Completed_v4

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -23,10 +23,11 @@ unzip rawdata.zip
 # 1. Create a directory named data
 mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
-mv rawdata data/raw
+mv rawdata data/raw/
 # 3. List the contents of the ./data/raw directory
 ls data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+mkdir data/processed
 mkdir data/processed/server_logs
 mkdir data/processed/user_logs
 mkdir data/processed/event_logs
