07dd11f8a2a63bd1b15ded1edf0a2f7d9d4a8345
Dion <dionysius.indraatmadja@gmail.com>
2024-11-23 09:50:29 -0500

    finished part 6

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -32,7 +32,8 @@ mkdir -p ./data/processed/server_logs ./data/processed/user_logs ./data/processe
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cp ./data/raw/*server*.log ./data/processed/server_logs  # copy server log files to server_log folder
 # 6. Repeat the above step for user logs and event logs
-
+cp ./data/raw/*user*.log ./data/processed/user_logs  # copy user log files to user_logs folder
+cp ./data/raw/*event*.log ./data/processed/event_logs  # copy event log files to event_logs folder
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
