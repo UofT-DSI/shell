9defeb64743afd58ecefddd7a3fa7bf265503c18
Dion <dionysius.indraatmadja@gmail.com>
2024-11-23 09:51:10 -0500

    finished part 8

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -37,7 +37,7 @@ cp ./data/raw/*event*.log ./data/processed/event_logs  # copy event log files to
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm ./data/processed/user_logs/*ipaddr* ./data/raw/*ipaddr*  # delte ipaddr files in both directories
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
+ls -R ./data/processed > ./data/inventory.txt  # -R recursively list all subfolders and files under ./data/processed. 
 
 
 ###########################################
