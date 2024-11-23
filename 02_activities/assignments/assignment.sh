0492f290dfbe81a7717f1192276c9e18bd7f3d0f
Pavandeep93 <kaurpavandeep93@gmail.com>
2024-11-22 20:55:42 -0500

    Assignment

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -24,7 +24,7 @@ unzip rawdata.zip
 mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw
-Move rawdata dataraw
+mv rawdata dataraw
 
 # 3. List the contents of the ./data/raw directory
 ls data/raw
