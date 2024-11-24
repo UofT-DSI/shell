89e6d40a0ad692f56b12a343372fb617fc6e2334
SueeeCC <chnchinwg@gmail.com>
2024-11-24 15:37:40 -0500

    complete

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -45,6 +45,7 @@ find ./data/processed/user_logs -type f -name "*ipaddr*" -exec rm -f {} \;
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 git checkout -b coworker-changes
 find ./data/processed -type f > ./data/inventory.txt
+
  
 
 
