c84967758e13d787d279c114e4a8e8496ccad40e
Chinyere <chinyere.ajohnson@gmail.com>
2024-11-23 23:50:33 -0500

    Updated errors

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -38,7 +38,7 @@ cp ./data/raw/server_log_*.log ./data/processed/server_logs/
 
 # 6. Repeat the above step for user logs and event logs
 
-cp ./data/raw/user*.log ./data/processed/user_logs/
+cp ./data/raw/user_log_* ./data/processed/user_logs/
 cp ./data/raw/event_log_*.log ./data/processed/event_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
