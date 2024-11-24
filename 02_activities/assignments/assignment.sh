8a52dbebb14f561c2bd73b4675b2788fd5841574
SueeeCC <chnchinwg@gmail.com>
2024-11-24 14:22:35 -0500

    ≈

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -26,7 +26,7 @@ mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
 
 # 3. List the contents of the ./data/raw directory
-
+ls 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
@@ -36,7 +36,8 @@ mkdir data
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
+touch ./data/inventory.txt
+ls 
 
 
 ###########################################
