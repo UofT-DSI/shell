48e50e7a9e29dc8ad5dea5830b757af56cd29057
SueeeCC <147413439+SueeeCC@users.noreply.github.com>
2024-11-24 16:37:39 -0500

    Update assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -35,7 +35,7 @@ unzip rawdata.zip
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
+coworker-changes branch
 
 
 ###########################################
