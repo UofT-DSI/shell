fc82ccca6144f14fb1bd7677b7097f521639a493
SueeeCC <chnchinwg@gmail.com>
2024-11-24 15:10:42 -0500

    complete list contents

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -27,7 +27,7 @@ mv ./rawdata ./data/raw
 # 2. Move the ./rawdata directory to ./data/raw
 
 # 3. List the contents of the ./data/raw directory
-ls 
+ls ./data/raw 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
