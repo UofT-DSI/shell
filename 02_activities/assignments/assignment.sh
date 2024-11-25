1e14a2a98ef25ad7623dca43c141c7c3b6385f4d
SueeeCC <chnchinwg@gmail.com>
2024-11-24 20:06:52 -0500

    complete

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -22,22 +22,25 @@ unzip rawdata.zip
 
 # 1. Create a directory named data
 mkdir data
-mv ./rawdata ./data/raw
 
 # 2. Move the ./rawdata directory to ./data/raw
-cd data
+mv ./rawdata ./data/raw
+
 # 3. List the contents of the ./data/raw directory
-ls .data/raw
+ls ./data/raw
+
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-touch server_logs 
-touch user_logs
-touch event_logs
+mkdir -p ./data/processed/server_logs ./data/processed/user_logs ./data/processed/event_logs
+
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+find ./data/raw -type f -name "*server*.log" -exec cp {} ./data/processed/server_logs/ \;
 
 # 6. Repeat the above step for user logs and event logs
+find ./data/raw -type f -name "*server*.log" -exec cp {} ./data/processed/server_logs/ \;
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+find ./data/raw -type f -name "*user*.log" -exec cp {} ./data/processed/user_logs/ \;
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
