1499e10e43402345d3e70e88eafc96a4097685e9
anisess10 <bouwazra.anis@gmail.com>
2024-12-01 11:01:32 -0500

    Revert "resolved the conflict"
    This reverts commit 24ac2430bd1f6a18486abcb4d29c9e199e18edef, reversing
    changes made to 8f5bf85ce5660f1db332aea27a509cfb20daffed.

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -11,7 +11,6 @@ set -x
 
 mkdir analysis output
 touch README.md
-echo "# Project Name: DSI Consulting Inc." > README.md
 touch analysis/main.py
 
 # download client data
@@ -39,8 +38,8 @@ $ cp ./data/raw/*server*.log ./data/processed/server_logs
 $ cp ./data/raw/*server*.log ./data/processed/event_logs
 $ cp ./data/raw/*server*.log ./data/processed/user_logs
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rf -rf ./data
-
+$ ls ./data/raw/*ipaddr*
+$ rm ./data/raw/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 $ touch ./data/inventory.txt
 $ find ./data/processed -type f
