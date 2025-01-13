0dcc6e850bba6b8b79c4daf233dc9051b8ad6050
Yitong Zhao <yit.zhao@mail.utoronto.ca>
2025-01-13 14:12:46 -0500

    I made changes in assignment.sh to complete my assignment.

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -21,23 +21,59 @@ unzip rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
+mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw
+mkdir raw
+cd ..
+mv rawdata ./data/raw
 
 # 3. List the contents of the ./data/raw directory
+cd ./data/raw
+ls
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+cd ..
+mkdir processed
+cd processed
+mkdir server_logs user_logs event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+cd ..
+cp raw/rawdata/server*.log processed/server_logs # I believe there're not .log files in raw but in a subfolder rawdata, because rawdata files were all moved there.
 
 # 6. Repeat the above step for user logs and event logs
+cp raw/rawdata/user*.log processed/user_logs # I believe there're not .log files in raw but in a subfolder rawdata, because rawdata files were all moved there.
+cp raw/rawdata/event*.log processed/event_logs # I believe there're not .log files in raw but in a subfolder rawdata, because rawdata files were all moved there.
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+rm -v raw/rawdata/*ipaddr*
+rm -v processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
+cd ..
+cd data
+touch inventory.txt
+cd processed
+ls
+
+cd user_logs
+ls >> inventory.txt
+
+cd processed
+cd event_logs
+ls >> inventory.txt
 
 
+cd processed
+cd server_logs
+ls >> inventory.txt
+
+cat inventory.txt
 
 ###########################################
 
 echo "Project setup is complete!"
+
+git commit assignment.sh -m "Assignment completed"
+
