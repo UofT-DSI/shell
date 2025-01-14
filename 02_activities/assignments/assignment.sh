8d20873676230f2b5d3086476aa628a24334bbf6
aleckhachatryan <a.khachatryan@mail.utoronto.ca>
2025-01-14 18:26:30 -0500

    commits to assignment

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -41,7 +41,7 @@ cp data/raw/event*.log data/processed/event_logs
 rm data/raw/*ipaddr*.*
 rm data/processed/user_logs/*ipaddr*.*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-ls data/processed >data/inventory.txt
+ls data/processed/*.* >data/inventory.txt
 
 ###########################################
 
