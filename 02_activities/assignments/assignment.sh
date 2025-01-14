75974104c19df2220402edc77c1a9b156f65b2bd
liliywu <liliana.wu@mail.utoronto.ca>
2025-01-14 12:04:46 -0500

    sorry realized what I did wrong

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -38,7 +38,7 @@ cp ./data/raw/*server*.log ./data/processed/server_logs/
 cp ./data/raw/*user*.log ./data/processed/user_logs/
 cp ./data/raw/*event*.log ./data/processed/event_logs/
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-<<<<<<< HEAD
+#<<<<<<< HEAD
 rm ./data/raw/*ipaddr* 
 rm ./data/processed/user_logs/*ipaddr*
 =======
@@ -47,7 +47,9 @@ rm ./data/processed/user_logs/*ipaddr*
 >>>>>>> ea20676d33161a6f4d0fcd3c4f7aa5360f0f4309
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 #hello!!
-touch ./data/processed/server_logs/*.log ./data/processed/user_logs/*.log ./data/processed/event_logs/*.log > ./data/inventory.txt
+touch ./data/processed/server_logs/*.log  > ./data/inventory.txt
+touch ./data/processed/user_logs/*.log . >> ./data/inventory.txt
+touch ./data/processed/event_logs/*.log >> ./data/inventory.txt
 #################################################
 
 echo "Project setup is complete!"
