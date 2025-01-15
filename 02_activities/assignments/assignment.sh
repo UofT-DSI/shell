18b3f845f04eb267cc938b259f159c7b052bdb34
Pooja Nathan <poojavasanthi@gmail.com>
2025-01-16 01:09:48 +0530

    fixed code that deleted ./data

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -44,7 +44,6 @@ cp ./data/raw/*event*.log ./data/processed/event_logs/
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm ./data/raw/*ipaddr*
 rm ./data/processed/user_logs/*ipaddr*
-rf -rf ./data
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 ls -R ./data/processed > ./data/inventory.txt
