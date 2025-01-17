2b87b7e1ad7c510f736826a7d3eeb4126dea1ea4
Yitong Zhao <yit.zhao@mail.utoronto.ca>
2025-01-17 11:55:13 -0500

    Updated my assignment because there were errors.

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -27,33 +27,32 @@ mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw
 mkdir raw
-cd ..
-mv rawdata ./data/raw
+mv rawdata raw
 
 # 3. List the contents of the ./data/raw directory
 cd ./data/raw
 ls
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-cd ..
+cd ./data
 mkdir processed
 cd processed
 mkdir server_logs user_logs event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cd ..
-cp raw/rawdata/server*.log processed/server_logs # I believe there're not .log files in raw but in a subfolder rawdata, because rawdata files were all moved there.
+cd ..
+cp raw/rawdata/server*.log data/processed/server_logs # I believe there're not .log files in raw but in a subfolder rawdata, because rawdata files were all moved there.
 
 # 6. Repeat the above step for user logs and event logs
-cp raw/rawdata/user*.log processed/user_logs # I believe there're not .log files in raw but in a subfolder rawdata, because rawdata files were all moved there.
-cp raw/rawdata/event*.log processed/event_logs # I believe there're not .log files in raw but in a subfolder rawdata, because rawdata files were all moved there.
+cp raw/rawdata/user*.log data/processed/user_logs # I believe there're not .log files in raw but in a subfolder rawdata, because rawdata files were all moved there.
+cp raw/rawdata/event*.log data/processed/event_logs # I believe there're not .log files in raw but in a subfolder rawdata, because rawdata files were all moved there.
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm -v raw/rawdata/*ipaddr*
 rm -v processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-cd ..
 cd data
 touch inventory.txt
 cd processed
@@ -62,7 +61,7 @@ ls
 cd user_logs
 ls >> inventory.txt
 
-cd processed
+cd ..
 cd event_logs
 ls >> inventory.txt
 
