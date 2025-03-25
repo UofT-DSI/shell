ab567afe6bbe6320652b0d67dbc53293e8c28121
Simeon Wong <dtxe@users.noreply.github.com>
2025-03-25 12:34:25 -0400

    bugfix: PR not working post instead of put

--- 03_instructional_team/autograder/autograder.py
+++ 03_instructional_team/autograder/autograder.py
@@ -322,13 +322,13 @@ if github_token:
 
     if correct == total:
         # also approve the PR
-        requests.put(
+        requests.post(
             f"https://api.github.com/repos/{github_repo_owner}/{github_repo_name}/pulls/{github_pr_number}/reviews",
             json={"event": "APPROVE", "body": "## Autograder results\n" + render_md },
             headers=headers)
     else:
         # request changes to the PR
-        requests.put(
+        requests.post(
             f"https://api.github.com/repos/{github_repo_owner}/{github_repo_name}/pulls/{github_pr_number}/reviews",
             json={"event": "REQUEST_CHANGES", "body": "## Autograder results\n" + render_md + "\n\nPlease address the issues listed above." },
             headers=headers)
