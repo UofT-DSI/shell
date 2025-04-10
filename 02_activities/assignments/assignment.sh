ad99260da931b6cb1819969566a60886bed191c6
ian-jinkun <zhaojinkunian@gmail.com>
2025-04-09 20:40:19 -0400

    assignment - first commit

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -21,7 +21,7 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
-
+mkdir add
 # 2. Move the ./rawdata directory to ./data/raw
 
 # 3. List the contents of the ./data/raw directory
