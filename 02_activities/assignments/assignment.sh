d6cf966bf61a9d62e20f7e4f5e81af6efa05b106
abhijit-mazumdar <pankumazumdar@gmail.com>
2025-04-10 18:42:53 -0400

    Added mkdir data/new

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -24,6 +24,7 @@ unzip -q rawdata.zip
 # 1. Create a directory named data
 mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
+mkdir ./data/raw
 cp ./rawdata/* ./data/raw
 # 3. List the contents of the ./data/raw directory
 ls ./data/raw
