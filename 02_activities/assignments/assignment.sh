928096bdc2a1f66c499f10d8b435b09509d767c9
Lindsey <lingxi.w95@gmail.com>
2025-04-09 20:24:08 -0400

    Assignment test run

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -21,7 +21,7 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
-
+mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
 
 # 3. List the contents of the ./data/raw directory
