073c31a8fc7c5a3a978cb6cdac1b52649000038e
Simeon <me@simeon.dev>
2025-04-11 11:47:26 -0400

    10th attempt

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -23,7 +23,7 @@ unzip -q rawdata.zip
 # 1. Create a directory named data
 mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
-mkdir data/raw
+mkdir -p data/raw
 
 # 3. List the contents of the ./data/raw directory
 
