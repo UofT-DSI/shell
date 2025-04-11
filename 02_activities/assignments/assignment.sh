9778421fcd2f5b7862d7930321fd2701949cccbb
ian-jinkun <zhaojinkunian@gmail.com>
2025-04-10 20:51:29 -0400

    commit all assignment job

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -37,9 +37,11 @@ cp ./data/raw/*server*.log ./data/processed/server_logs/
 cp ./data/raw/*user*.log ./data/processed/user_logs/
 cp ./data/raw/*event*.log ./data/processed/event_logs/
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rm ./data/raw/*ipaddr*.log && rm ./data/raw/*ipaddr*.log
+rm ./data/raw/*ipaddr* 
+rm ./data/processed/user_logs/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
+touch ./data/inventory.txt 
+ls ./data/processed > ./data/inventory.txt
 
 ###########################################
 
