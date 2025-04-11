0d40d9b7e9209c31db67f5e580ce6cc2e01c9aa9
Simeon <me@simeon.dev>
2025-04-11 11:54:07 -0400

    12 attempts!

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -23,8 +23,8 @@ unzip -q rawdata.zip
 # 1. Create a directory named data
 mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
-mkdir -p /data/raw
-
+mkdir -p data/raw
+mv rawdata data/raw 
 # 3. List the contents of the ./data/raw directory
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
