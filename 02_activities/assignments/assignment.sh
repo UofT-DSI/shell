6f8d2256f81ffda5e9e300743d9fd9db269566c3
Adriana <adrianameraz@outlook.com>
2025-04-12 11:27:48 -0400

    updated3

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -27,9 +27,9 @@ mv ./rawdata ./data/raw/
 # 3. List the contents of the ./data/raw directory
 ls data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-$ mkdir data/processed data/processed/server_logs User_logs event_logs
+mkdir data/processed/ server_logs User_logs event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cp  ./data/raw/*server*.log ./data/processed/server_logs/
+cp ./data/raw/*server*.log ./data/processed/server_logs/
 # 6. Repeat the above step for user logs and event logs
 cp  ./data/raw/*server*.log ./data/processed/user_logs/
 cp  ./data/raw/*server*.log ./data/processed/event_logs/
