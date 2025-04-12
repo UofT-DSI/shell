fd45afff3e5316a704ed8c52e62609da10de524f
Simeon <me@simeon.dev>
2025-04-11 23:26:35 -0400

    new

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -45,7 +45,7 @@ rm data/raw/*ipaddr*
 rm data/processed/user_logs/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 touch data/inventory.txt    
-ls data/processed > data/inventory.txt
+ls ./data/processed > data/inventory.txt
 
 
 ###########################################
