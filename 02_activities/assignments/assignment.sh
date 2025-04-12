4dc6badbaceea14cf57ad4eb24c0e9055d0d092e
User1 <mrhenry.wong@alumni.utoronto.ca>
2025-04-12 10:29:44 -0400

    9th commit

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -54,10 +54,11 @@ mkdir "./data/processed"
 # mkdir "server_logs" "user_logs" "event_logs"
 
 # 4d. recreate the folders again > not sure why the autograder is rejecting?
-mkdir data/processed/server_logs data/processed/user_logs data/processed/event_logs
+
 mkdir data/processed/server_logs
 mkdir data/processed/user_logs
 mkdir data/processed/event_logs
+mkdir data/processed/server_logs data/processed/user_logs data/processed/event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 # 5a. Change directory
