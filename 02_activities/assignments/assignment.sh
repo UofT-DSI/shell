f896b87f79859f3fc2259ce72619d86da755335b
Michelle <minnovation.l@gmail.com>
2025-04-12 13:25:36 -0400

    Complete the assignment script update

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -21,20 +21,48 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
+cd /Users/michelleliu/dsi_shell/02_activities/assignments
+mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw
+mv ./rawdata ./data/raw
 
 # 3. List the contents of the ./data/raw directory
+cd ./data/raw
+ls 
+
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 
+cd /Users/michelleliu/dsi_shell/02_activities/assignments/data/processed
+mkdir server_logs user_logs event_logs
+
+
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+cd ..
+cd raw
+cp ./*server*.log  ../processed/server_logs
+
 
 # 6. Repeat the above step for user logs and event logs
+cp ./*user*.log ../processed/user_logs
+cp ./*event*.log ../processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 
+rm -r ./*ipaddr* 
+cd ../processed/user_logs
+rm -r ./*ipaddr*
+
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
+cd /Users/michelleliu/dsi_shell/02_activities/assignments/data/processed/event_logs
+ls -a >/Users/michelleliu/dsi_shell/02_activities/assignments/data/inventory.txt
+cd /Users/michelleliu/dsi_shell/02_activities/assignments/data/processed/server_logs
+ls -a >>/Users/michelleliu/dsi_shell/02_activities/assignments/data/inventory.txt
+cd /Users/michelleliu/dsi_shell/02_activities/assignments/data/processed/user_logs
+ls -a >>/Users/michelleliu/dsi_shell/02_activities/assignments/data/inventory.txt
+
+
 
 
 ###########################################
