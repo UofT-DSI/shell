61d769dd0899c247e336499f65631e73489923d7
Simeon <me@simeon.dev>
2025-04-12 09:07:46 -0400

    saturday

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -24,7 +24,7 @@ unzip -q rawdata.zip
 mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
 mkdir -p data/raw
-mv rawdata/* data/raw
+mv rawdata data/raw
 # 3. List the contents of the ./data/raw directory
 ls data/raw
 
@@ -45,7 +45,7 @@ rm data/raw/*ipaddr*
 rm data/processed/user_logs/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 touch data/inventory.txt    
-ls ./data/processed > data/inventory.txt
+ls /data/processed > data/inventory.txt
 
 
 ###########################################
