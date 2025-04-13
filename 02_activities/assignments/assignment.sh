15748b8de051daca2a00afc3e14c7e02ec4acf28
Simeon <me@simeon.dev>
2025-04-13 10:02:49 -0400

    sunday 6

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -43,6 +43,7 @@ cp data/raw/*event*.log data/processed/event_logs
 rm data/raw/*ipaddr*
 rm data/processed/user_logs/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
+touch data/inventory.txt
 find ./data/processed -type f > ./data/inventory.txt
 
 ###########################################
