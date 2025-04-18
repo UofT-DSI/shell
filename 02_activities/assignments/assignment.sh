2e7cbc768932587afa6b33faafde3d41c22edc60
ayaAllan <35306519+ayaAllan@users.noreply.github.com>
2025-04-19 00:27:09 +0100

    completed part 1: updated script to move downloaded logs to respective folders and remove any logs that may contain sensitive information

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -21,21 +21,30 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
+mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw
+mv rawdata data/raw
 
 # 3. List the contents of the ./data/raw directory
+ls data/raw
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+cd data
+mkdir -p processed/server_logs processed/user_logs processed/event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+mv raw/server_*.log processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
+mv raw/user_*.log processed/user_logs
+mv raw/event_*.log processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+rm raw/*ipaddr* processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
+ls -R processed > inventory.txt
 
 ###########################################
 
