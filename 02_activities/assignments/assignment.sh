68b0ed732ea91dfc2ac9e9bd07b0444f5c992359
jsparks6 <jennifer.sparks@mail.utoronto.ca>
2025-04-20 22:19:48 -0400

    Update assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -28,71 +28,75 @@ mkdir data
 
 cd data
 mkdir raw
-mv ../rawdata ./raw
+cd data
+mv ./rawdata ./raw
 
 # 3. List the contents of the ./data/raw directory
 
 cd raw
-cd rawdata
 ls
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 
+cd data
 mkdir processed
-ls
+mv ./processed ./data
 cd processed
-mkdir server_logs user_logs event_logs
+mkdir server_logs 
+mkdir user_logs 
+mkdir event_logs
+mv ./server_logs ./data/processed
+mv ./user_logs ./data/processed
+mv ./event_logs ./data/processed
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 
-cd ..
-cd ..
-cp data/raw/rawdata/*server*.log data/processed/server_logs/
-cd processed/server_logs
-ls
+cd ./data/raw
+cp ./data/raw/*server*_.log/* ./data/processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
 
-cd ..
-cd ..
-cd ..
-cp data/raw/rawdata/*user*.log data/processed/user_logs/
-cd data/processed/user_logs
-ls
-cd ..
-cd ..
-cd ..
-cp data/raw/rawdata/*event*.log data/processed/event_logs/
-cd data/processed/event_logs
-ls
+cd ./data/raw
+cp ./data/raw/*user*_.log/* ./data/processed/server_logs
+
+cd ./data/raw
+cp ./data/raw/*event*_.log/* ./data/processed/server_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 
-cd ..
-cd ..
-cd ..
-cd data/raw/rawdata
-rm *ipaddr*
-cd ..
-cd ..
-cd ..
-rm ./data/processed/user_logs/ *ipaddr*
-cd data/processed/user_logs
-ls 
-cd ..
-cd event_logs
-cd ..
-cd user_logs
-cd ..
-cd server_logs
+cd ./data/raw
+rm ./data/raw/*ipadder* 
+cd ./data/processed/user_logs
+rm ./data/processed//user_logs/*ipadder*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
-cd ..
-cd ..
-cd processed/
-code inventory.txt
+cd ./data
+touch inventory.txt
+mv ./inventory.txt ./data 
+cd ./data/processed
+cp .data/processed/* ./inventory.txt
+cd .inventory.text
+cat ./inventory.txt
 
 ###########################################
+
+#commit changes#
+git commit assignment.sh -m "corrections made to assignment 1 and resubmitted on april 20, 2025"
+
+#pull updates from coworkers#
+git pull http://github.com/UofT-DSI/shell coworker-changes --no-rebase
+
+#check for conflicts#
+git status
+
+#once conflicts are resolved, commit the merge#
+git commit
+
+#double check by re-testing script# 
+bash assignment.sh
+
+#commit final changes in my assignment branch#
+git status
 
 echo "Project setup is complete!"
