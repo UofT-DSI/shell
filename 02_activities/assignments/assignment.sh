abafed11ec84dff600ecc5869c9669e447041b36
Murad <muradmontasir@hotmail.com>
2025-04-22 18:50:28 -0400

    removed faulty code from coworker

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -49,7 +49,7 @@ cp data/raw/*user*.log data/processed/user_logs/
 cp data/raw/*event*.log data/processed/event_logs/  
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rf -rf ./data
+
 
 rm -f data/raw/*ipaddr*     
 rm -f data/processed/user_logs/*ipaddr*
