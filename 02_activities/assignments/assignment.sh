f2b99da23fc7a7910496de1021ce70fd75926a4b
Shahab Malik <shahab.a.malik@gmail.com>
2025-07-30 19:57:23 -0400

    Test commit

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -28,11 +28,11 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
-
+mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
-
+mv -r ./rawdata ./data/raw
 # 3. List the contents of the ./data/raw directory
-
+ls ./data/raw/raw directory
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
