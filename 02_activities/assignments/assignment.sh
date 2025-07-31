b26c0bc6c1e28c7822028dcad5ad23c65cdf294a
Christina Lamparter <cl.lamparter@gmail.com>
2025-07-30 20:39:36 -0400

    MOre progress on assignment - still not done.

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -29,9 +29,16 @@ unzip -q rawdata.zip
 
 # 1. Create a directory named data
 
+mkdir data
+
 # 2. Move the ./rawdata directory to ./data/raw
 
+mv ./rawdata ./data/raw
+
 # 3. List the contents of the ./data/raw directory
+
+cd data/raw
+ls  
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 
