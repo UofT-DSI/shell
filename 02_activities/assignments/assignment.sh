41a1b48c904ec62854b8fe0e6d8f2e573eb26e49
PreethiSiva1 <preethisk96@gmail.com>
2025-07-31 14:58:26 -0400

    copying server logs from data to processed/server_logs

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -45,6 +45,10 @@ pwd
 mkdir server_logs user_logs event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+cd ../
+pwd
+ls
+cp raw/*server*.log processed/server_logs/
 
 # 6. Repeat the above step for user logs and event logs
 
