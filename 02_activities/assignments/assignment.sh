eed4ada5ca126510c98b79d3474d730aef620a36
PreethiSiva1 <preethisk96@gmail.com>
2025-07-31 12:54:00 -0400

    creating server_logs,, user_logs and event_logs under data/processed folder

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -37,6 +37,10 @@ mv -v rawdata data/raw
 ls -l data/raw
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+cd newprojects/data
+mkdir processed
+cd processed
+mkdir server_logs user_logs event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 
