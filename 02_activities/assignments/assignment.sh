22fc9a18725c348f93d60d5cc9462054aacace00
PreethiSiva1 <preethisk96@gmail.com>
2025-07-30 20:31:32 -0400

    Moving the files from rawdata directory to /data/raw

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -32,7 +32,7 @@ mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw
 
-
+mv ./raw data.zip ./data/raw
 # 3. List the contents of the ./data/raw directory
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
