b1e4256c60906c4a12e7c81f034041ec693486da
Hamed Abangar <h_abangar@yahoo.com>
2025-08-01 11:49:02 -0400

    Edited and fixed the issue. All commands working fine

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -34,6 +34,7 @@ mv ./rawdata ./data/raw
 # 3. List the contents of the ./data/raw directory
 ls ./data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+mkdir ./data/processed
 mkdir ./data/processed/server_logs ./data/processed/user_logs ./data/processed/event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cp ./data/raw/*server*.*log* ./data/processed/server_logs
