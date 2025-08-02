270d1e657e3894cdd3a7b322df0e7d03f486e03a
Sanyam <sanyamarora0099@gmail.com>
2025-08-02 14:56:18 -0400

    updated answers in assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -11,7 +11,7 @@ set -x
 
 if [ -d newproject ]; then
   echo "Directory 'newproject' already exists. Please remove it before running this script."
-  exit 1
+ # exit 1
 fi
 mkdir newproject
 cd newproject
@@ -28,21 +28,29 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
+mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw
+mv rawdata/ data/raw
 
 # 3. List the contents of the ./data/raw directory
+$ ls /c/Users/Sanyam/NewData/shell/02_activities/assignments/newproject/data/raw
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+mkdir server_logs user_logs event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+ cp ./*server*.log /c/Users/Sanyam/NewData/shell/02_activities/assignments/newproject/data/processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
+cp ./*user*.log /c/Users/Sanyam/NewData/shell/02_activities/assignments/newproject/data/processed/user_logs   ;
+   cp ./*event*.log /c/Users/Sanyam/NewData/shell/02_activities/assignments/newproject/data/processed/event_logs 
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-
+rm *ipaddr* 
+rm /c/Users/Sanyam/NewData/shell/02_activities/assignments/newproject/data/processed/user_logs/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
+ls processed/* > inventory.txt
 
 ###########################################
 
