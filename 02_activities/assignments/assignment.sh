f044f4f75e117ee841ad2618690fb78b23e6e8aa
Shahab Malik <shahab.a.malik@gmail.com>
2025-08-02 10:37:35 -0400

    added the mkdir data/raw command

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -30,7 +30,8 @@ unzip -q rawdata.zip
 # 1. Create a directory named data
 mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
-mv -R rawdata/ data/raw/
+mkdir data/raw
+mv -R rawdata data/raw/
 # 3. List the contents of the ./data/raw directory
 ls ./data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
