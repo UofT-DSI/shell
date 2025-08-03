dd4e6b5bda8d19cb62657ed5260f64c69ef8ee0e
ashwinder1 <ashwinder221+github@gmail.com>
2025-08-02 21:26:45 -0400

    assignment-module-shell-by-ashwinder1

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -13,8 +13,8 @@ if [ -d newproject ]; then
   echo "Directory 'newproject' already exists. Please remove it before running this script."
   exit 1
 fi
-mkdir newproject
-cd newproject
+mkdir ~/dsi/newproject
+cd ~/dsi/newproject
 
 mkdir analysis output
 touch README.md
@@ -28,21 +28,35 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
+mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw
+mv ./rawdata ./data/raw/
 
 # 3. List the contents of the ./data/raw directory
+ls ./data/raw
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+mkdir -p ./data/processed/
+cd ./data/processed
+mkdir server_logs user_logs event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+cd ..
+cp -r ./raw/*server*.log ./processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
+cp -r ./raw/*user*.log ./processed/user_logs
+cp -r ./raw/*event*.log ./processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+rm -r ./raw/*ipaddr*
+rm -r ./processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
+ls ./processed/event_logs/ > ./inventory.txt
+ls ./processed/user_logs/ >> ./inventory.txt
+ls ./processed/server_logs/ >> ./inventory.txt
 
 ###########################################
 
