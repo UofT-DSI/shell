3fdf79cb514ccce546add1a30fdf07b556945263
lianml2025 <isabelle.lian@alumni.utoronto.ca>
2025-08-03 16:21:28 -0400

    Update assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -47,10 +47,9 @@ mkdir server_logs user_logs event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 ls -l ./data/raw
-cd ../data/raw/rawdata
+cd raw
 pwd
-cp *server*.log ./data/raw/
-cp ./data/raw/rawdata/*server*.log .data/processed/server_logs/
+cp /rawdata/*server*.log .data/processed/server_logs/
 # 6. Repeat the above step for user logs and event logs
 cp raw/rawdata/*user*.log processed/user_logs/
 cp raw/rawdata/*event*.log processed/event_logs/
