14bd98a35fee87424d9ae2bbe039609e93fa138e
lianml2025 <isabelle.lian@alumni.utoronto.ca>
2025-08-03 17:28:06 -0400

    Update assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -47,7 +47,6 @@ mkdir server_logs user_logs event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cd ..
-cd data
 pwd
 find ./raw -type f -name '*server*.log' -exec cp {} ./processed/server_logs/ \;
 
