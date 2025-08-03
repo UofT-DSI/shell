dd671bb1ca8f2a551092296802c639ea39c1dd65
samojoo <sam.ojo@outlook.com>
2025-08-03 13:33:42 -0400

    Final assignment

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -16,7 +16,7 @@ fi
 mkdir newproject
 cd newproject
 
-mkdir analysis output
+mkdir analysis outputls
 touch README.md
 touch analysis/main.py
 
@@ -24,25 +24,41 @@ touch analysis/main.py
 curl -Lo rawdata.zip https://github.com/UofT-DSI/shell/raw/refs/heads/main/02_activities/assignments/rawdata.zip
 unzip -q rawdata.zip
 
-###########################################
+#####################..######################
 # Complete assignment here
 
 # 1. Create a directory named data
 mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
-
+mv ./rawdata ./data/raw
 # 3. List the contents of the ./data/raw directory
-
+ls ./data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+cd ./data
+mkdir processed
+cd processed
+mkdir server_logs user_logs event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+cd ../raw
+cp *server*.log ../processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
+cp *user*.log ../processed/user_logs
+cp *event*.log ../processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-
+rm *ipaddr*
+cd ../processed/user_logs
+rm *ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
+cd ..
+cd ..
+touch inventory.txt
+ls ./processed/server_logs >> inventory.txt
+ls ./processed/user_logs >> inventory.txt
+ls ./processed/event_logs >> inventory.txt
+cat inventory.txt
 
 ###########################################
 
