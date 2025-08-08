5bc7b4ea7434480195735ca45a1d6369a8c97270
Toronto3000 <bakary3000@gmail.com>
2025-08-08 09:30:35 -0400

    correction_2

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -43,20 +43,20 @@ mkdir -p data/processed/event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
       
-cp ./data/raw/*server*.log  ./processed/server_logs
+cp ./data/raw/*server*.log  ./data/processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
-cp ./data/raw/*server*.log  ./data/processed/user_logs
-cp ./data/raw/*server*.log  ./data/processed/event_logs
+cp ./data/raw/*user*.log  ./data/processed/user_logs/
+cp ./data/raw/*event*.log  ./data/processed/event_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm ./data/raw/*ipaddr*
-rm ./data/processed/*ipaddr*
+rm ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
-code ./data/inventory.tx
-ls > ./data/inventory.txt
+find data/processed -type f > data/inventory.txt
+
 ###########################################
 
 echo "Project setup is complete!"
