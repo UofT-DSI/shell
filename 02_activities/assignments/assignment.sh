7b9458960c22bc7e250840295ee10342e7dbee59
Vignash <vignash46@gmail.com>
2025-10-05 20:43:32 -0400

    Update assignment.sh with latest changes after testing

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -9,6 +9,10 @@ set -x
 # project name and a brief description of the project.
 # Then it unzips the raw data provided by the client.
 
+if [ -d newproject ]; then
+  echo "Directory 'newproject' already exists. Please remove it before running this script."
+  exit 1
+fi
 mkdir newproject
 cd newproject
 
