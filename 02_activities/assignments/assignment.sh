e642e559cf69994235305135893d46580ca04597
pca <jiax.zhang@mail.utoronto.ca>
2025-10-05 22:05:31 -0400

    Update my assignment

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -47,8 +47,8 @@ cp data/raw/*server*.log data/processed/server_logs/
 
 
 # 6. Repeat the above step for user logs and event logs
-cp data/raw/*user.log data/processed/user_logs/
-cp data/raw/*event.log data/procssed/event_logs/
+cp data/raw/*user*.log data/processed/user_logs/
+cp data/raw/*event*.log data/processed/event_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm -f data/raw/*ipaddr*
