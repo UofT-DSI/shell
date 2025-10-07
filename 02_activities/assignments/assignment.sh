9cb69b20091ae4ed5eba3e211b2ae1e9016cb48f
elinakad <elina.kadriu@mail.utoronto.ca>
2025-10-07 16:33:41 -0400

    assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -25,6 +25,47 @@ curl -Lo rawdata.zip https://github.com/UofT-DSI/shell/raw/refs/heads/main/02_ac
 unzip -q rawdata.zip
 
 ###########################################
+# 1. Create a directory named data
+cd ~/Desktop
+cd shell
+cd 02_activities
+cd assignments
+cd newproject
+mkdir data
+cd data
+mkdir raw 
+
+# 2. Move the ./rawdata directory to ./data/raw
+cd raw
+mv ../../rawdata* .
+
+# 3. List the contents of the ./data/raw directory
+ls 
+
+# 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+cd ..
+mkdir processed
+cd processed
+mkdir server_logs user_logs event_logs 
+
+# 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+cd server_logs
+cp ../../raw/rawdata/server_*.log .
+
+# 6. Repeat the above step for user logs and event logs
+cd ..
+cd event_logs
+cp ../../raw/rawdata/event_*.log .
+cd ..
+cd user_logs
+cp ../../raw/rawdata/user_*.log .
+rm user_ipaddr_*.log ../../raw/rawdata/user_ipaddr_*.log ../../raw/rawdata/ipaddr_*.txt
+
+# 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
+cd ../..
+touch inventory.txt
+ls ./processed/*_logs/*.log > inventory.txt
+###########################################
 # Complete assignment here
 
 # 1. Create a directory named data
