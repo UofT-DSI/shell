dc075aceee26f998844cdc87ec9be1c207d0de21
Ziyi Dai <ziyidai@Ziyis-MacBook-Pro.local>
2025-10-08 10:10:29 -0600

    assignment

--- assignment.sh
+++ assignment.sh
@@ -28,9 +28,9 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
-mkdir data
+mkdir ./data
 # 2. Move the ./rawdata directory to ./data/raw
-mv rawdata data/raw
+mv ./rawdata data/raw
 # 3. List the contents of the ./data/raw directory
 ls data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
