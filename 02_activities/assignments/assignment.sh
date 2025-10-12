4654b7ee0593b574013e73b995cfe610220c5bc7
Tanveer <tanveer.rouf@gmail.com>
2025-10-12 14:40:37 -0400

    Resolve merge conflict markers

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -48,12 +48,12 @@ cp data/raw/*user*.log data/processed/user_logs/
 cp data/raw/*event*.log data/processed/event_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-<<<<<<< HEAD
+
 rm data/raw/*ipaddr*
 rm data/processed/user_logs/*ipaddr*
-=======
+
 rf -rf ./data
->>>>>>> ea20676d33161a6f4d0fcd3c4f7aa5360f0f4309
+
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 ls data/processed/*/* > data/inventory.txt
