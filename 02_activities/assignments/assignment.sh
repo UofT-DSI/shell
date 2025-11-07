427653c6ac5b56111024d68e95caaaea14d433c8
Cookie Jars <cookiejars8@users.noreply.github.com>
2025-11-06 19:28:51 -0500

    hope this works!

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -28,7 +28,7 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
-mkdir data
+mkdir data1
 # 2. Move the ./rawdata directory to ./data/raw
 
 # 3. List the contents of the ./data/raw directory
