80450fcd5deb67522165ede648bfb17cef533783
Nicole Yeung <nicole-demo@users.noreply.github.com>
2025-11-07 15:07:38 -0500

    Completed all tasks

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -31,19 +31,37 @@ unzip -q rawdata.zip
 mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw
+mv ./rawdata ./data/raw
 
 # 3. List the contents of the ./data/raw directory
+ls ./data/raw
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+cd data
+mkdir processed
+cd processed
+mkdir server_logs user_logs event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+cd ~/dsi3/shell/02_activities/assignments/newproject
+cp ./data/raw/*server*.log ./data/processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
+cp ./data/raw/*server*.log ./data/processed/user_logs
+cp ./data/raw/*server*.log ./data/processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+cd ./data/raw/
+rm *ipaddr*
+
+cd ~/dsi3/shell/02_activities/assignments/newproject
+cd ./data/processed/user_logs
+rm *ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
+cd ~/dsi3/shell/02_activities/assignments/newproject/data
+touch inventory.txt
+ls ./processed/* >> inventory.txt
 
 ###########################################
 
