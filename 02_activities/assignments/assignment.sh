e7e4ae79c01eb2cdcd3e923d30db7d71c9bfecc2
Cookie Jars <cookiejars8@users.noreply.github.com>
2025-11-09 12:26:18 -0500

    committing again

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -35,15 +35,18 @@ mv ./rawdata ./data/raw
 ls ./data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 cd ./data/processed
-mkdir server_logs
-mkdir user_logs
-mkdir event_logs
+mkdir data/processed/server_logs
+mkdir data/processed/user_logs
+mkdir data/processed/event_logs
 ls
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cp -vi ./data/raw/*server*.log ./data/processed/server_logs/
+cp ./data/raw/*server
+cp ./data/raw/*.log 
+mv ./data/raw/*server ./data/processed/server_logs/
+mv .data/raw/*.log ./data/processed/server_logs/
 
 # 6. Repeat the above step for user logs and event logs
-cp -vi ./data/raw/*user*.log ./data/processed/user_logs/
+cp ./data/raw/*user*.log ./data/processed/user_logs/
 cp -vi ./data/raw/*event*.log ./data/processed/event_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
