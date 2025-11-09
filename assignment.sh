f1aee63c24c35f214816ec6249c5b44a38f78917
Jyoti2001 <jyotishukla0195@gmail.com>
2025-11-09 15:24:46 -0500

    Final assignment with correcting mistakes

--- assignment.sh
+++ assignment.sh
@@ -9,36 +9,28 @@ set -x
 # project name and a brief description of the project.
 # Then it unzips the raw data provided by the client.
 
-if [ -d newproject ]; then
-  echo "Recreating the newproject directory"
-  rm -rf newproject
-fi
-mkdir newproject
-cd newproject
 
-mkdir analysis output
-touch README.md
-echo "# Project Name: UoT DSI Bash assignment." > README.md
-touch analysis/main.py
 
 # download client data
-curl -Lo rawdata.zip https://github.com/UofT-DSI/shell/raw/refs/heads/main/02_activities/assignments/rawdata.zip
+curl -Lo rawdata.zip https://github.com/Jyoti2001/shell/raw/refs/heads/main/02_activities/assignments/rawdata.zip
 unzip -q rawdata.zip
 
 ###########################################
 # Complete assignment here
 
 # 1. Create a directory named data
-mkdir data
+mkdir -p data
+mkdir -p data/raw
 # 2. Move the ./rawdata directory to ./data/raw
-mv ./rawdata ./data/raw
+mv ./rawdata/* ./data/raw/ 2>/dev/null
+rmdir .,rawdata 2>/dev/null
 # 3. List the contents of the ./data/raw directory
 ls ./data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 mkdir ./data/processed
-mkdir ./data/processed/server_logs
-mkdir ./data/processed/user_logs
-mkdir ./data/processed/event_logs
+mkdir -p ./data/processed/server_logs
+mkdir -p ./data/processed/user_logs
+mkdir -p ./data/processed/event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cp ./data/raw/server*.log ./data/processed/server_logs
 # 6. Repeat the above step for user logs and event logs
