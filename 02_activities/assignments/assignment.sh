6699a0162375d98c9625980b68c2e1a977e9ebb7
Catalina Barboza Solis <catalina.barbozasolis@telusagcg.com>
2026-02-09 13:39:10 -0700

    Try 2

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -43,7 +43,7 @@ cp ./data/raw/*server*.log ./data/processed/server_logs/
 
 # 6. Repeat the above step for user logs and event logs
 cp ./data/raw/*user*.log ./data/processed/user_logs/
-cp ./data/raw/*event*.log ./data/processed/events_logs/
+cp ./data/raw/*event*.log ./data/processed/event_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 find ./data/raw ./data/processed/user_logs -type f -name "*ipaddr*" -delete
