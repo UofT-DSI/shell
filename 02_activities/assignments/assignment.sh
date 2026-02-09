7b68cc1cc1d3ed485885c053f8de45300d499eeb
jason <luhao19900625@gmail.com>
2026-02-08 19:52:26 -0500

    commit assignment and push by Jason Hao Lu

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -28,20 +28,30 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
+mkdir -p data
 
 # 2. Move the ./rawdata directory to ./data/raw
+mv rawdata data/raw
 
 # 3. List the contents of the ./data/raw directory
+ls -la data/raw
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+mkdir -p data/processed/server_logs data/processed/user_logs data/processed/event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+find data/raw -type f -name '*server*.log' -exec cp -t data/processed/server_logs {} +
 
 # 6. Repeat the above step for user logs and event logs
+find data/raw -type f -name '*user*.log'  -exec cp -t data/processed/user_logs {} +
+find data/raw -type f -name '*event*.log' -exec cp -t data/processed/event_logs {} +
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+find data/raw -type f -name '*ipaddr*' -delete
+find data/processed/user_logs -type f -name '*ipaddr*' -delete
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
+find data/processed -type f > data/inventory.txt
 
 
 ###########################################
