eb01f945ce50248a72dc51615027f8dfd2baee01
Emily Zohar <emily.zohar@mail.utoronto.ca>
2026-02-26 16:39:10 -0500

    Emily Zohar's assignment one is complete

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -28,21 +28,39 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
+mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw (eg. move it into ./data and rename it to raw)
+mv rawdata data/raw
 
 # 3. List the contents of the ./data/raw directory
+cd ./data/raw
+ls
 
 # 4. Create the directory ./data/processed, 
 #    then create the following sub-directories within it: server_logs, user_logs, and event_logs
+cd ..
+mkdir processed
+cd processed
+mkdir server_logs 
+mkdir user_logs
+mkdir event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+cd ..
+mv ./raw/server*.log ./processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
+mv ./raw/user*.log ./processed/user_logs
+mv ./raw/event*.log ./processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+rm ./raw/*ipaddr*
+rm ./processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
+cd ..
+ls ./data/processed/* > inventory.txt
 
 
 ###########################################
