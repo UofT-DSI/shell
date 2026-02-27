3ed5438ca4c5cf918a926cb58494a0d348bd3027
celiazhu-uoft <zhiyue.zhu@mail.utoronto.ca>
2026-02-27 18:20:56 -0500

    assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -28,43 +28,38 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
-zhuzhiyue@Zhus-MacBook-Air ~ % cd ~
-zhuzhiyue@Zhus-MacBook-Air ~ % git clone https://github.com/celiazhu-uoft/shell.git  
-zhuzhiyue@Zhus-MacBook-Air ~ % git switch -c assignment
-zhuzhiyue@Zhus-MacBook-Air ~ % cd Desktop
-zhuzhiyue@Zhus-MacBook-Air Desktop % unzip rawdata.zip
-zhuzhiyue@Zhus-MacBook-Air Desktop % mkdir data
+mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw (eg. move it into ./data and rename it to raw)
-zhuzhiyue@Zhus-MacBook-Air Desktop % mv ./rawdata ./data/raw
+mv ./rawdata ./data/raw
 
 # 3. List the contents of the ./data/raw directory
-zhuzhiyue@Zhus-MacBook-Air Desktop % ls ./data/raw
+ls ./data/raw
 
 # 4. Create the directory ./data/processed, 
 #    then create the following sub-directories within it: server_logs, user_logs, and event_logs
-zhuzhiyue@Zhus-MacBook-Air Desktop % mkdir ./data/processed
-zhuzhiyue@Zhus-MacBook-Air Desktop % cd data
-zhuzhiyue@Zhus-MacBook-Air data % cd processed
-zhuzhiyue@Zhus-MacBook-Air processed % mkdir server_logs user_logs event_logs
+mkdir ./data/processed
+cd data
+cd processed
+mkdir server_logs user_logs event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-zhuzhiyue@Zhus-MacBook-Air processed % cd ..
-zhuzhiyue@Zhus-MacBook-Air data % cd ..
-zhuzhiyue@Zhus-MacBook-Air Desktop % cp ./data/raw/*server*.log ./data/processed/server_logs/
+cd ..
+cd ..
+cp ./data/raw/*server*.log ./data/processed/server_logs/
 
 # 6. Repeat the above step for user logs and event logs
-zhuzhiyue@Zhus-MacBook-Air Desktop % cp ./data/raw/*user*.log ./data/processed/user_logs/
-zhuzhiyue@Zhus-MacBook-Air Desktop % cp ./data/raw/*event*.log ./data/processed/event_logs/
+cp ./data/raw/*user*.log ./data/processed/user_logs/
+cp ./data/raw/*event*.log ./data/processed/event_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-zhuzhiyue@Zhus-MacBook-Air Desktop % rm ./data/raw/*ipaddr* ./data/processed/user_logs/*ipaddr*
+rm ./data/raw/*ipaddr* ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-zhuzhiyue@Zhus-MacBook-Air Desktop % touch ./data/inventory.txt
-zhuzhiyue@Zhus-MacBook-Air Desktop % ls -p ./data/processed/*/ | grep -v / > ./data/inventory.txt
+touch ./data/inventory.txt
+ls -p ./data/processed/*/ | grep -v / > ./data/inventory.txt
 
 ###########################################
 
-echo "Project setup is complete!"
-zhuzhiyue@Zhus-MacBook-Air Desktop % echo "Project setup is complete"
+#echo "Project setup is complete!"
+echo "Project setup is complete"
