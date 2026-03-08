a785c4b6bf2e03199f0ad7bcfa837b6011878891
Bruna Viana <bruna.prunes.viana@mail.utoronto.ca>
2026-03-08 15:46:20 -0400

    removing space in order to open a new pull request (merged the last one accidentally)

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -68,7 +68,6 @@ rm data/raw/*ipaddr*
 rm data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
 find data/processed -type f > data/inventory.txt
 
 
