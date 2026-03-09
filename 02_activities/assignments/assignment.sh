4b44f1dd7b8f14138db4ac7bc70410164bc80af8
Bruna Viana <bruna.prunes.viana@mail.utoronto.ca>
2026-03-08 20:10:28 -0400

    first commit

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -18,6 +18,7 @@ cd newproject
 
 mkdir analysis output
 touch README.md
+echo "# Project Name: DSI Consulting Inc." > README.md
 touch analysis/main.py
 
 # download client data
@@ -29,20 +30,45 @@ unzip -q rawdata.zip
 
 # 1. Create a directory named data
 
+mkdir data
+
 # 2. Move the ./rawdata directory to ./data/raw (eg. move it into ./data and rename it to raw)
 
+mkdir rawdata
+
+mv rawdata data/raw
+
 # 3. List the contents of the ./data/raw directory
+
+ls data/raw
 
 # 4. Create the directory ./data/processed, 
 #    then create the following sub-directories within it: server_logs, user_logs, and event_logs
 
+mkdir data/processed
+
+mkdir data/processed/server_logs
+mkdir data/processed/user_logs
+mkdir data/processed/event_logs
+
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+
+cp data/raw/*server*.log data/processed/server_logs/
 
 # 6. Repeat the above step for user logs and event logs
 
+cp data/raw/*user*.log data/processed/user_logs/
+cp data/raw/*event*.log data/processed/event_logs/
+
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+rf -rf ./data
+
+rm data/raw/*ipaddr*
+rm data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
+
+find data/processed -type f > data/inventory.txt
 
 
 ###########################################
