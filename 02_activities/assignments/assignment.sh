e168d97d6c77afe32638c277b9d4e5c6a73c649e
Meghan Hewton <mlhewton@gmail.com>
2026-04-14 00:52:33 -0400

    Committing. Better late than never?

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -43,23 +43,18 @@ cd processed
 mkdir server_logs user_logs event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cd ..
-cd raw
-cp server*.log ~/Desktop/'DSI Foundations'/Session3_Git/shell/newproject/data/processed/server_logs
+cp ./raw/server*.log ./processed/server_logs
 # 6. Repeat the above step for user logs and event logs
-cp user_l*.log ~/Desktop/'DSI Foundations'/Session3_Git/shell/newproject/data/processed/user_logs
-cp event_l*.log ~/Desktop/'DSI Foundations'/Session3_Git/shell/newproject/data/processed/event_logs
+cp ./raw/*user_l*.log ./processed/user_logs
+cp ./raw/*event*.log ./processed/event_logs
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+cd raw
 rm *ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 cd ..
-cd processed/server_logs
-ls >> ~/Desktop/'DSI Foundations'/Session3_Git/shell/newproject/data/inventory.txt
-cd ..
-cd user_logs
-ls >> ~/Desktop/'DSI Foundations'/Session3_Git/shell/newproject/data/inventory.txt
-cd ..
-cd event_logs
-ls >> ~/Desktop/'DSI Foundations'/Session3_Git/shell/newproject/data/inventory.txt
+ls processed/server_logs >> inventory.txt
+ls processed/user_logs >> inventory.txt
+ls processed/event_logs >> inventory.txt
 ###########################################
 
 echo "Project setup is complete!"
