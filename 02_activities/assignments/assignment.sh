eaabd9a669f61b2dd4b960d259e7992b887dafb9
Keli Chiu <keli.chiu.kc@gmail..com>
2026-07-23 15:31:44 -0400

    completed assignment

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -13,6 +13,7 @@ if [ -d newproject ]; then
   echo "Recreating the newproject directory"
   rm -rf newproject
 fi
+
 mkdir newproject
 cd newproject
 
@@ -20,30 +21,42 @@ mkdir analysis output
 touch README.md
 touch analysis/main.py
 
-# download client data
-curl -Lo rawdata.zip https://github.com/UofT-DSI/shell/raw/refs/heads/main/02_activities/assignments/rawdata.zip
+# Download client data
+curl -Lo rawdata.zip \
+  https://github.com/UofT-DSI/shell/raw/refs/heads/main/02_activities/assignments/rawdata.zip
+
 unzip -q rawdata.zip
 
 ###########################################
 # Complete assignment here
 
 # 1. Create a directory named data
+mkdir data
 
-# 2. Move the ./rawdata directory to ./data/raw (eg. move it into ./data and rename it to raw)
+# 2. Move the ./rawdata directory to ./data/raw
+mv rawdata data/raw
 
 # 3. List the contents of the ./data/raw directory
+ls data/raw
 
-# 4. Create the directory ./data/processed, 
-#    then create the following sub-directories within it: server_logs, user_logs, and event_logs
+# 4. Create processed directories and their subdirectories
+mkdir -p data/processed/server_logs
+mkdir -p data/processed/user_logs
+mkdir -p data/processed/event_logs
 
-# 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+# 5. Copy all server log files
+cp data/raw/*server*.log data/processed/server_logs/
 
-# 6. Repeat the above step for user logs and event logs
+# 6. Copy all user and event log files
+cp data/raw/*user*.log data/processed/user_logs/
+cp data/raw/*event*.log data/processed/event_logs/
 
-# 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+# 7. Remove files containing IP addresses
+rm -f data/raw/*ipaddr*
+rm -f data/processed/user_logs/*ipaddr*
 
-# 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
+# 8. Create an inventory of all files in the processed subfolders
+find data/processed -type f > data/inventory.txt
 
 ###########################################
 
