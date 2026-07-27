2e65f67c482fdafd1ddf82523e92b522e20a5ff3
shima <shim.sdt8@gmail.com>
2026-07-26 23:50:19 -0400

    complete my assignment

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -29,22 +29,39 @@ unzip -q rawdata.zip
 
 # 1. Create a directory named data
 
+Answer1: mkdir data
+
 # 2. Move the ./rawdata directory to ./data/raw (eg. move it into ./data and rename it to raw)
 
+Answer2: mv rawdat data/raw
+
 # 3. List the contents of the ./data/raw directory
+
+Answer3: ls data/raw
 
 # 4. Create the directory ./data/processed, 
 #    then create the following sub-directories within it: server_logs, user_logs, and event_logs
 
+Answer4: mkdir data/processed
+         mkdir processed/server_logs processed/user_logs processed/event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+
+Answer5: cp raw/server_log*.log processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
 
+Answer6: cp raw/user_log*.log processed/user_logs
+         cp raw/user_ipaddr*.log processed/user_logs
+         cp raw/event_log*.log processed/event_logs
+
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+
+Answer7: rm raw/user_ipaddr*.log
+         rm processed/user_logs/user_ipaddr*.log
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
-
+Answer8: ls -R data/processed > data/inventory.txt
 ###########################################
 
 echo "Project setup is complete!"
